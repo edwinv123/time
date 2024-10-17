5a1961f1fd1f50463bf25d698ae3c1308bb16265
edwinv123 <126evalentinnunez@frhsd.com>
2024-10-17 13:30:36 -0400

    changeee

--- js/indwx.js
+++ js/indwx.js
@@ -1,2 +1,13 @@
 const d = new Date();
 let hour = d.getHours();
+
+console.log(hour);
+
+if (hour < 12) {
+    // Morning
+    } else if (hour < 17) {
+    // Afternoon
+    }
+    else {
+    // Night
+    }
