01f587a39abcd84bd96faf3dfa5d4015311b990d
edwinv123 <126evalentinnunez@frhsd.com>
2024-10-28 11:56:38 -0400

    time

--- js/indwx.js
+++ js/indwx.js
@@ -4,10 +4,21 @@ let hour = d.getHours();
 console.log(hour);
 
 if (hour < 12) {
-    // Morning
+    //Morning
+    document.getElementById("greeting").innerText = "Good Morning";
+    document.getElementById("image").src = "img/images.jpg";
     } else if (hour < 17) {
     // Afternoon
+    document.getElementById("greeting").innerText = "Good Afternoon";
+    document.getElementById("image").src = "img/afternoon.jpg";
     }
     else {
     // Night
+    document.getElementById("greeting").innerText = "Good Night";
+    document.getElementById("image").src = "img/chamoy.jpg";
     }
+
+//    let greetngElement = document.getElementById("greeting");
+// console.log(greetingElement);
+
+// document.getElementById("greeting").innerText = "Good Night";
