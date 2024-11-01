18baec159105e9570d8664338b369559f5ff0da8
edwinv123 <126evalentinnunez@frhsd.com>
2024-11-01 12:53:41 -0400

    changeeee

--- js/indwx.js
+++ js/indwx.js
@@ -1,22 +1,30 @@
 const d = new Date();
 let hour = d.getHours();
-
-console.log(hour);
-
+//hour=1
+//hour=15
+//hour=19
+// console.log(hour);
+let greetingText = document.getElementById("greeting-text");
+hour=19
 if (hour < 12) {
-    //Morning
-    document.getElementById("greeting").innerText = "Good Morning";
-    document.getElementById("image").src = "img/images.jpg";
+    // if hour is less than 12pm
+    greetingText.textContent = "Good Morning!";
+    document.querySelector(".night").classList.add("d-none");
+    document.querySelector(".afternoon").classList.add("d-none");
     } else if (hour < 17) {
-    // Afternoon
-    document.getElementById("greeting").innerText = "Good Afternoon";
-    document.getElementById("image").src = "img/";
-    }
-    else {
-    // Night
-    document.getElementById("greeting").innerText = "Good Night";
-    document.getElementById("image").src = "img/chamoy.jpg";
-    }
+        // if hour is less than 17:00 (but greater than 12pm)
+        greetingText.textContent = "Good Afternoon";
+        document.getElementsByTagName("body")[0].classList.add("noon-bg");
+        greetingText.parentElement.classList.add("noon-gradient");
+        document.querySelector(".morning").classList.add("d-none");
+        document.querySelector(".night").classList.add("d-none");
+      }
+      else {
+        greetingText.textContent = "Good Night";
+        greetingText.parentElement.classList.add("night-gradient");
+        document.querySelector(".morning").classList.add("d-none");
+        document.querySelector(".afternoon").classList.add("d-none");
+      }
 
 //    let greetngElement = document.getElementById("greeting");
 // console.log(greetingElement);
